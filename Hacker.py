89d373e850500e6580d0512ee4cf2c62ed263998
joe <jonjy036@mymail.unisa.edu.au>
2025-10-12 23:56:32 +0800

    Create 'hacker_list' to allow 'find_target' method.
    Set initial def statements for methods with TBC comments.
    
    Import random library to use in 'find_target'.

--- Hacker.py
+++ Hacker.py
@@ -7,14 +7,18 @@ Username: JONJY036
 This is my own work as defined by the University's Academic Misconduct Policy.
 """
 from Asset import Asset
+import random
 
 # Define class, __init__ and __str__
 class Hacker:
+    hacker_list = []
+
     def __init__(self, name: str):
         self.__name = name
         self.__inventory = []
         self.__rig = None
         self.__trace_level = 0
+        Hacker.hacker_list.append(self)
 
         crypto_token = Asset('Crypto Token', 'Digital currency used to purchase rigs')
 
@@ -45,3 +49,34 @@ class Hacker:
         self.__trace_level = trace_level
 
     # Define Methods
+    def aquire_rig(self):
+        # TBC
+
+    def launch_data_spike(self, target):
+        # TBC
+
+    @classmethod
+    def find_target(cls):
+        # TBC
+
+    def extract_unsecured_assets(self, rig):
+        # TBC
+
+    def encrypt_asset(self):
+        # TBC
+
+    def decrypt_asset(self):
+        # TBC
+
+    def upgrade_rig(self):
+        # TBC
+
+    def scan_inventory(self):
+        # TBC
+
+    def store_asset(self, rig_name, asset):
+        # TBC
+
+    def retrieve_asset(self, rig_name, asset):
+        # TBC
+
