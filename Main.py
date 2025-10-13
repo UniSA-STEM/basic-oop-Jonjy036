4c81cb6ef2f28022c464052450e0d56969eede57
joe <jonjy036@mymail.unisa.edu.au>
2025-10-13 16:43:52 +0800

    Add first set of test code.

--- Main.py
+++ Main.py
@@ -7,6 +7,23 @@ Username: JONJY036
 This is my own work as defined by the University's Academic Misconduct Policy.
 """
 from Hacker import Hacker
-from Rig import Rig
-from Asset import Asset
+#from Rig import Rig
+#from Asset import Asset
 
+# Basic early checks on Hacker Instantiation, aquire_rig,
+# find_target and launch_data_spike methods.
+hacker1 = Hacker('Joe')
+print(str(hacker1))
+
+hacker2 = Hacker('JONJY036')
+hacker3 = Hacker('JOEYCEZROO')
+
+hacker1.aquire_rig()
+hacker2.aquire_rig()
+hacker3.aquire_rig()
+
+hacker1.find_target()
+hacker2.find_target()
+hacker3.find_target()
+
+hacker1.launch_data_spike()
